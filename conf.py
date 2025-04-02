1350992cbc5ee5f7b86cb70a61d4aed22b159ec4
Jean-Pierre Forcioli <jpforcioli@fortinet.com>
2025-04-02 07:13:42 +0200

    Logo change

--- conf.py
+++ conf.py
@@ -72,7 +72,7 @@ images_config = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-DOC_LOGO = "fortimanager_api.png"
+DOC_LOGO = "fortimanager_api_howto.png"
 html_static_path = ['_static']
 html_theme_options = {
     "announcement": "How to FortiManager API?",
